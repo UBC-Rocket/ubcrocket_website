37365e584a6b4350b9f90be31a1648bf0b737581
dayshaunlee <leedayshaun@gmail.com>
2024-08-02 06:47:11 -0700

    incorporated npm and scss for neater file structure

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # ubcrocket_website
-A website for UBC Rocket, a design team at the University of British Columbia.
+A website for [UBC Rocket](https://www.ubcrocket.com/), a design team at the [University of British Columbia](https://www.ubc.ca/).
+
+## Compiled from various [Bootstrap 5](https://getbootstrap.com/) templates from [Start Bootstrap](https://startbootstrap.com/), modified
+- [Modern Business](https://startbootstrap.com/template/modern-business) (rough draft for pages)
+- [Agency](https://startbootstrap.com/theme/agency) (home page masthead and heading fonts)
+- [Clean Blog Jekyll](https://startbootstrap.com/theme/clean-blog-jekyll) (navigation and Bootstrap/SCSS stack)
+
+### Other tools used
+- [Bootstrap 3.0 Org-Chart by Lee Owens](https://github.com/owens2024/bootstrap-orgchart) (org chart - modified to integrate with rest of page)
--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,46 @@
 /*!
-* Start Bootstrap - Modern Business v5.0.7 (https://startbootstrap.com/template-overviews/modern-business)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-modern-business/blob/master/LICENSE)
+* Start Bootstrap - Clean Blog v5.1.0 (https://startbootstrap.com/theme/clean-blog)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-clean-blog/blob/master/LICENSE)
 */
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+(function ($) {
+    "use strict"; // Start of use strict
+
+    // Floating label headings for the contact form
+    $("body").on("input propertychange", ".floating-label-form-group", function (e) {
+        $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+    }).on("focus", ".floating-label-form-group", function () {
+        $(this).addClass("floating-label-form-group-with-focus");
+    }).on("blur", ".floating-label-form-group", function () {
+        $(this).removeClass("floating-label-form-group-with-focus");
+    });
+
+    // Show the navbar when the page is scrolled up
+    var MQL = 992;
+
+    //primary navigation slide-in effect
+    if ($(window).width() > MQL) {
+        var headerHeight = $('#mainNav').height();
+        $(window).on('scroll', {
+                previousTop: 0
+            },
+            function () {
+                var currentTop = $(window).scrollTop();
+                //check if user is scrolling up
+                if (currentTop < this.previousTop) {
+                    //if scrolling up...
+                    if (currentTop > 0 && $('#mainNav').hasClass('is-fixed')) {
+                        $('#mainNav').addClass('is-visible');
+                    } else {
+                        $('#mainNav').removeClass('is-visible is-fixed');
+                    }
+                } else if (currentTop > this.previousTop) {
+                    //if scrolling down...
+                    $('#mainNav').removeClass('is-visible');
+                    if (currentTop > headerHeight && !$('#mainNav').hasClass('is-fixed')) $('#mainNav').addClass('is-fixed');
+                }
+                this.previousTop = currentTop;
+            });
+    }
+
+})(jQuery); // End of use strict
